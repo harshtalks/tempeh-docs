ed5e6dfa4b520a6085b48260a7e4d7420476946c
Harsh Pareek <harshpareek@Harshs-MacBook-Air.local>
2024-05-03 23:36:22 +0530

    feat: init

---
+++ vocs.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vocs";
+
+export default defineConfig({
+  title: "Tempeh",
+  sidebar: [
+    {
+      text: "Getting Started",
+      link: "/",
+    },
+    {
+      text: "Routes",
+      link: "/route",
+    },
+  ],
+});
