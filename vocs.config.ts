ce3c2e5dcfee90c0a199eb33e3979f4c3b1f5284
Harsh <harshpareek91@gmail.com>
2024-10-24 20:26:52 +0530

    fix: home paqge

--- vocs.config.ts
+++ vocs.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "vocs";
 
 export default defineConfig({
-  title: "Tempeh",
+  title: "Tempeh 🧀",
   sidebar: [
     {
       text: "Getting Started",
       link: "/",
+    },
+    {
+      text: "About Tempeh",
+      link: "/description",
     },
     {
       text: "Router Config API",
@@ -32,4 +36,31 @@ export default defineConfig({
       link: "/update",
     },
   ],
+  description:
+    "Type-safe, declarative, and flexible routing for Next.js applications.",
+  editLink: {
+    pattern: "https://github.com/harshtalks/tempeh-docs",
+    text: "Suggest changes to this page",
+  },
+  socials: [
+    {
+      icon: "github",
+      link: "https://github.com/harshtalks/tempeh",
+    },
+    {
+      icon: "x",
+      link: "https://x.com/harshicorp",
+    },
+  ],
+  topNav: [
+    { text: "Tempeh CLI", link: "https://github.com/harshtalks/tempeh-cli" },
+    {
+      text: "Docs Github",
+      link: "https://github.com/harshtalks/tempeh-docs",
+    },
+    { text: "Get Started", link: "/description" },
+  ],
+  ogImageUrl: {
+    "/": "https://res.cloudinary.com/hp-creative/image/upload/v1729781355/Image_from_imgsrc_5_hjy9qk.png",
+  },
 });
